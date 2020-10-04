01c72fad6f36db74037b6afcbb92e2572e393923
Galina Muzychenko <muzychenko.galina@gmail.com>
2020-10-04 12:00:16 +0300

    init : start task count-cat and what-season

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix ) {
+ 
+  var arrStr= matrix.join('-').split('^^');
+
+return (arrStr.length > 0)? (arrStr.length - 1 ): 0;
+  
   // remove line with error and write your code here
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  seasonName=['winter', 'spring', 'summer', 'autumn'];
+  var  message = null; 
+  var mounth = date.getMonth();
+
+  try {
+
+  if (mounth <= 1 || mounth == 11) {
+    message = seasonName[0];
+  } else if (mounth <= 4 && mounth>1) {
+    message = seasonName[1];
+  } else if (mounth <= 7 && mounth>4) {
+    message = seasonName[2];
+  } else {
+    message = seasonName[3];
+  }
+
+} catch(err) {
+ 
+      message = 'THROWN';
+
+}
+return message;
 };
